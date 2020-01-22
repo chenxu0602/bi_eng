db235f48d3567e8651cafdf7bc1284b737e8afff
Chen Xu <chenxu@Chens-iMac.local>
2020-01-23 01:24:04 +0800

    first

---
+++ sample.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import pandas as pd
+
+# Edit the two paths to vanorder and vaninterest csv files
+vanorder_path = '/path/to/vanorder.csv'
+vaninterest_path = '/path/to/vaninterest.csv'
+
+# Read the two csv into pandas DataFrame, parsing the datetime columns
+vanorder = pd.read_csv(vanorder_path, parse_dates=['order_datetime', 'txCreate'])
+vaninterest = pd.read_csv(vaninterest_path, parse_dates=['txCreate'])
